bb0ea0400348b0fbe281ac03c1a8b6dc53c5598b
Derek Hu <derek@nuuo.com>
2018-08-23 23:44:08 +0800

    The function ReadInput didn't return true., caused the runtime error.
    Case closed.

--- uva10189/uva10189.cpp
+++ uva10189/uva10189.cpp
@@ -1,9 +1,8 @@
-using namespace std;
-
 #include <iostream>
 #include <vector>
 #include <list>
-#include <cstring>
+//#include <cstring>
+using namespace std;
 
 class MineSweeper {
     typedef struct pos {
@@ -16,81 +15,79 @@ class MineSweeper {
     int dimM;
     int dimN;
     int MapSize;
-    char answer[100 * 100];
+    vector<char> answer;
     vector<pos_type> MineMap;
-
-    void Initialize(int m, int n) {
-        dimM = m;
-        dimN = n;
-        //answer = new char[dimM*dimN];
-        //memset(answer, '0', sizeof(char)*dimM*dimN);
-        for (int i=0;i<dimM*dimN;i++) answer[i] = '0';
-    }
-
-    inline int index(int x, int y) {
-        if (x < 0 || y < 0 || x >= dimM || y >= dimN) return -1; 
-        else return x + dimM*y;
-    };
-
-    void MarkMinePosition(pos_type p) {
-        int minepos = index(p.x, p.y);
-        if (minepos >=0 && minepos < MapSize) answer[minepos] = '*';
-        for (int i = p.x - 1; i <= p.x + 1; i++)
-        {
-            for (int j = p.y - 1; j <= p.y + 1; j++)
-            {
-                int k = index(i, j);
-                if (k >= 0 && k < MapSize)
-                {
-                    if (answer[k] != '*' ) answer[k]++;
-                }
-            }
-        }
-    };
-
 public:
     MineSweeper(int cid) : CaseId(cid) {
     };
-    ~MineSweeper() {
-        //delete [] answer;
-    };
-
-    bool ReadInput(istream& in) {
-        int m, n = 0;
-        in >> n >> m;
-        if (m == 0 || n == 0) return false;
-
-        MapSize = m*n;
-        char c = '.';
-        for (int i = 0; i < MapSize; i++)
-        {
-            in >> c;
-            if (c == '*') MineMap.push_back(pos_type(i % m, i / m));
-        }
-
-        Initialize(m, n);
-    };
-
-    const void CalcAnswer() {
-        vector<pos_type>::iterator it = MineMap.begin();
-        while (it != MineMap.end()) {
-            MarkMinePosition(*it);
-            it++;
-        }
-    };
-    const void PrintResult(ostream& out) {
-        if (CaseId >= 2) out << endl;
-        out << "Field #" << CaseId << ":" << endl;
-        int i = 0;
-        while (true)
-        {
-            out << answer[i];
-            if ((i + 1) % dimM == 0) out << endl;
-            
-            if (++i == MapSize) break;
-        }
-    };
+    void MarkMinePosition(pos_type p);
+    bool ReadInput(istream& in);
+    void CalcAnswer();
+    void PrintResult(ostream& out);
+    int index(int x, int y);
 };
+
+int MineSweeper::index(int x, int y) {
+    if (x < 0 || y < 0 || x >= dimM || y >= dimN) return -1;
+    else return x + dimM*y;
+}
+
+void MineSweeper::MarkMinePosition(pos_type p) {
+    int minepos = index(p.x, p.y);
+    if (minepos >= 0 && minepos < MapSize) answer[minepos] = '*';
+
+    for (int i = p.x - 1; i <= p.x + 1; i++)
+    {
+        for (int j = p.y - 1; j <= p.y + 1; j++)
+        {
+            int k = index(i, j);
+            if (k >= 0 && k < MapSize)
+            {
+                if (answer[k] != '*') answer[k]++;
+            }
+        }
+    }
+}
+
+bool MineSweeper::ReadInput(istream& in) {
+    int m, n = 0;
+    in >> n >> m;
+    if (m == 0 || n == 0) return false;
+
+    MapSize = m*n;
+    char c = '.';
+    for (int i = 0; i < MapSize; i++)
+    {
+        in >> c;
+        if (c == '*') MineMap.push_back(pos_type(i % m, i / m));
+    }
+
+    dimM = m;
+    dimN = n;
+    answer = vector<char>(m*n, '0');
+
+    return true;
+}
+
+void MineSweeper::CalcAnswer() {
+    vector<pos_type>::iterator it = MineMap.begin();
+    while (it != MineMap.end()) {
+        MarkMinePosition(*it);
+        it++;
+    }
+}
+void MineSweeper::PrintResult(ostream& out) {
+    if (CaseId >= 2) out << endl;
+    out << "Field #" << CaseId << ":" << endl;
+    int i = 0;
+    while (true)
+    {
+        out << answer[i];
+        if ((i + 1) % dimM == 0) out << endl;
+            
+        if (++i >= MapSize) break;
+    }
+}
 
 
 int main()
